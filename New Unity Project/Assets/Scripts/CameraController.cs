25df2c3a2d02eda7ca4112e60c8ca16b8133d101
rreeves8 <rreeves8@uwo.ca>
2021-02-11 17:38:13 -0500

    Added fly down camera

---
+++ New Unity Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour{
+
+    public GameObject camera;
+
+
+    void Update(){
+
+        float time = Time.time + 0.067f;
+        
+        
+       // Debug.Log(time);
+        
+        if (!CameraPath.isCompleted(time)){
+
+            camera.transform.position = new Vector3(0.0f, CameraPath.startPathZ(time), 0.0f);
+            camera.transform.Rotate(CameraPath.startPathRotationX(time), 0,0);
+        }
+    }
+}
+
---
+++ New Unity Project/Assets/Scripts/CameraPath.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPath : MonoBehaviour{
+    public static float previous;
+    public static bool firstrun = true;
+
+    public static float startPathZ(float time){
+        //1/x function
+        return 2f / time; 
+    }
+
+    public static float startPathRotationX(float time){
+        float slope;
+        float rad;
+        float newAngle;
+        float previousPlace;
+
+        if (firstrun){
+            slope = (2f / Mathf.Pow(time, 2));
+            rad = Mathf.Atan(slope);
+            newAngle = (rad * (180 / Mathf.PI));
+            previous = newAngle;
+            firstrun = false;
+            return newAngle;
+        }
+        else
+        {
+            slope = (2f / Mathf.Pow(time, 2));
+            rad = Mathf.Atan(slope);
+            newAngle = (rad * (180 / Mathf.PI));
+            previousPlace = previous;
+            previous = newAngle;
+        }
+        
+        return newAngle - previousPlace;
+    }
+
+
+    public static bool isCompleted(float time){
+
+        if(time >= 5f)
+        {
+            return true;
+        }
+         
+        return false;
+    }
+
+}
