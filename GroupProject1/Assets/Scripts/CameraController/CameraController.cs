7b2b54a26844fa260bb9d758ac24c73bfaa195d5
mveysey <71096754+mveysey@users.noreply.github.com>
2021-02-11 22:54:46 -0500

    Moved Camera Controller to GroupProject1
    Moved over the camera zoom from New Unity Project to GroupProject1.

---
+++ GroupProject1/Assets/Scripts/CameraController/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject camera;
+    public float stretch;
+    public float maxTime;
+
+    private CameraPath path;
+
+    void Start()
+    {
+        path = new CameraPath(stretch, camera.transform.position.y, maxTime);
+    }
+
+    void Update()
+    {   
+        if (!path.isCompleted()){
+            path.updateTime(Time.time);
+            camera.transform.position = new Vector3(0.0f, path.pathZ(), 0.0f);
+            camera.transform.Rotate(path.pathRotationX(), 0f, 0f);
+        }
+    }
+}
---
+++ GroupProject1/Assets/Scripts/CameraController/CameraPath.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPath
+{
+    private float previous;
+    private bool firstrun;
+    private float stretch;
+    private float starting_time;
+    private float time;
+    private float max_time;
+
+    public CameraPath(float stretch, float starting_height, float max_time)
+    {
+        this.firstrun = true;
+        this.starting_time = stretch / starting_height; 
+        this.stretch = stretch;
+        this.max_time = max_time;
+    }
+
+    public float pathZ()
+    {
+        //1/x function
+        return stretch / time;
+    }
+
+    public float pathRotationX()
+    {
+        //rotation is the angle of the tangent to 1/x at time
+        float slope = (stretch / Mathf.Pow(time, 2));
+        float rad = Mathf.Atan(slope);
+        float newAngle = (rad * (180 / Mathf.PI));
+        
+        float previousholder = previous;
+        previous = newAngle;
+        
+        if (firstrun)
+        {
+            firstrun = false;
+            return newAngle;
+        }
+        else
+        {
+            return newAngle - previousholder;
+        }
+    }
+
+    public void updateTime(float time)
+    {
+        this.time = time + starting_time;
+    }
+
+    public bool isCompleted()
+    {
+
+        if (time >= max_time)
+        {
+            return true;
+        }
+        return false;
+    }
+}
