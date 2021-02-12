c6c42258aea2e86741d30c2d7b2d88becd01e8d9
mveysey <71096754+mveysey@users.noreply.github.com>
2021-02-11 22:11:48 -0500

    Started Scene_1
    Created the bare minimum in terms of functionality and graphics. Mirthal now has dialogue when you click on the "Talk to Mirthal" button. Click on the "continue" button to trigger another sentence from the script.
    
    Still need to:
    -> Animate the text box so it doesn't appear at the start of the game (have it rise from the bottom)
    -> Re-work the code so MC is actually having a conversation with Mirthal (maybe create another queue for MC and time it with Mirthal's queue?)
    -> Getting the name of the MC will be difficult to implement if we want the text boxes to look nice, so that might be a bonus later...

---
+++ GroupProject1/Assets/Scripts/MirthalDialogue/Dialogue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Pass into dialogueManager when you want to display a new message
+
+[System.Serializable]
+public class Dialogue
+{
+    public string name;
+
+    [TextArea(1,20)]
+    public string[] mirthalSentences;
+}
