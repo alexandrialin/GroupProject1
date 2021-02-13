5dfb86f2037465aad2075d63c96b41787549421b
mveysey <71096754+mveysey@users.noreply.github.com>
2021-02-13 15:28:22 -0500

    Started intro/scene 1 updates
    Still need to:
    
    --> Update transitions of intro photos with fading
    --> Add backgrounds behind texts (in both scenes)
    --> Add music?? (free assets from asset store)

---
+++ GroupProject1/Assets/__Scripts/PlayPictures.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayPictures : MonoBehaviour
+{
+
+    public RawImage hometownImage;
+    public RawImage waterfallImage;
+    public GameObject textDisplay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DisplayImage());
+    }
+
+    IEnumerator DisplayImage()
+    {
+        hometownImage.enabled = true;
+        yield return new WaitForSeconds(3);
+        hometownImage.enabled = false;
+
+        waterfallImage.enabled = true;
+        textDisplay.SetActive(true);
+        yield return new WaitForSeconds(5);
+        waterfallImage.enabled = false;
+        textDisplay.SetActive(false);
+        
+    }
+}
