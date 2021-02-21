026bece876966addcfac1ff6abf9dc022228d41d
rreeves8 <rreeves8@uwo.ca>
2021-02-21 14:41:18 -0500

    Added scripts

---
+++ GroupProject1/Assets/__Scripts/Scene_3 Stuff/InterfaceRemoval.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InterfaceRemoval : MonoBehaviour
+{
+    public GameObject ContinueButton;
+
+    public GameObject UI;
+
+    private int count = 0;
+    
+    public void removeUI()
+    {
+        Debug.Log(count);
+        
+        if(count == 2)
+        {
+            UI.SetActive(false);
+        }
+        else
+        {
+            count++;
+        }
+    }
+
+}
